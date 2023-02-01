ff4b6612f50b921347a305525276d7a826a39cfc
dr.tuxer <38743836+dragonpilee@users.noreply.github.com>
2023-02-02 01:49:50 +0530

    Add files via upload

---
+++ Heart.java
@@ -0,0 +1,9 @@
+public class Heart {
+  public static void main(String[] args) {
+    System.out.println("   ***   ");
+    System.out.println(" *     * ");
+    System.out.println("(   ♥   )");
+    System.out.println(" *     * ");
+    System.out.println("   ***   ");
+  }
+}
